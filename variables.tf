37c65aa709c492c0fc4a5a6cf5e87e4397c73477
Muhammad Uzair Janjua <90286318+uzairjanjua1@users.noreply.github.com>
2024-06-04 01:03:06 +0200

    Add files via upload

--- variables.tf
+++ variables.tf
@@ -1,11 +1,23 @@
 variable "resource_group_name" {
   description = "The name of the resource group"
   type        = string
-  default     = "rg-aks-tr"   # Default name of the resource group
+  default     = "rg-aks-tr"
 }
 
 variable "location" {
   description = "The Azure region to deploy resources"
   type        = string
-  default     = "eastus"         # Default Azure region
+  default     = "eastus"
+}
+
+variable "sql_admin_username" {
+  description = "The administrator username for the SQL Server"
+  type        = string
+  default     = "sqladmin"
+}
+
+variable "sql_admin_password" {
+  description = "The administrator password for the SQL Server"
+  type        = string
+  default     = "Pakistan123!"
 }
